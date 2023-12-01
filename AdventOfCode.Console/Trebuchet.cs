755ed3ad5dfe6d4537b900c53712f663403eb205
Tomas Tamantini <tomasbft@gmail.com>
2023-12-01 20:29:18 -0300

    Finish day1/1st challenge

--- AdventOfCode.Console/Trebuchet.cs
+++ AdventOfCode.Console/Trebuchet.cs
@@ -1,13 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AdventOfCode.Console
+﻿namespace AdventOfCode.Console
 {
     public class Trebuchet
     {
-        static public int extractNumericValue(string value) { return 0; }
+        static public int ExtractNumericValue(string value)
+        {
+            var digits = value.Where(char.IsDigit).Select(digit => (int)char.GetNumericValue(digit));
+            int firstDigit = digits.FirstOrDefault();
+            int lastDigit = digits.LastOrDefault();
+            return firstDigit * 10 + lastDigit;
+        }
+
+        static public int AddUpNumericValues(IEnumerable<string> values)
+        {
+            return values.Select(ExtractNumericValue).Sum();
+        }
+
+        static public int AddUpNumericValuesFromTextFile(string path)
+        {
+            var values = File.ReadAllLines(path);
+            return AddUpNumericValues(values);
+        }
     }
 }
--- AdventOfCode.Tests/TestDay1Trebuchet.cs
+++ AdventOfCode.Tests/TestDay1Trebuchet.cs
@@ -8,8 +8,32 @@ namespace AdventOfCode.Tests
         public void TestStringWithNoDigitsAddsToZero()
         {
             var stringWithNoDigits = "abc";
-            var numericValue = Trebuchet.extractNumericValue(stringWithNoDigits);
+            var numericValue = Trebuchet.ExtractNumericValue(stringWithNoDigits);
             Assert.Equal(0, numericValue);
+        }
+
+        [Fact]
+        public void TestStringWithSingleDigitCorrespondsToThatDigitTwice()
+        {
+            var stringWithDigits = "sdfds4asdf";
+            var numericValue = Trebuchet.ExtractNumericValue(stringWithDigits);
+            Assert.Equal(44, numericValue);
+        }
+
+        [Fact]
+        public void TestStringWithTwoDigitsCorrespondToThoseTwoDigitsConcatenated()
+        {
+            var stringWithDigits = "sd1fd3sa";
+            var numericValue = Trebuchet.ExtractNumericValue(stringWithDigits);
+            Assert.Equal(13, numericValue);
+        }
+
+        [Fact]
+        public void TestListOfStringsHaveTheirValuesAddedUp()
+        {
+            var listOfStrings = new List<string> { "", "1", "sd1fd3sa", "0sr78", "4321" };
+            var numericValue = Trebuchet.AddUpNumericValues(listOfStrings);
+            Assert.Equal(73, numericValue);
         }
     }
 }
